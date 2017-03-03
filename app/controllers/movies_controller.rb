7b7699e27810c9ff44f38f0b5b87837d26d90998
nasifibn <your.nmajeed@uncc.edu>
2017-03-03 02:24:35 +0000

    Done with section 4.9

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,7 @@ class MoviesController < ApplicationController
   id = params[:id] # retrieve movie ID from URI route
   @movie = Movie.find(id) # look up movie by unique ID
   # will render app/views/movies/show.html.haml by default
-   @movie = Movie.where(:id => params[:id])
+   ###@movie = Movie.where(:id => params[:id])
   end
   
   def new
